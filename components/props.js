fef7cbe7509df8d292477da2103e10f7ac5eaf30
Francisco Salcines <francisco.salcines@colex.local>
2021-04-14 09:39:03 +0200

    fin sección 2

--- components/props.js
+++ components/props.js
@@ -1,23 +1,29 @@
 Vue.component('props', {
-    props: ['name', 'surname', 'age'],
-
-    /*name: {
+    props: {
+      name: {
         type: String,
         required: true
+      },
+      surname: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number,
+        required: true,
+        validator: value => {
+          if (value < 18) {
+            console.warn('No eres mayor de edad...');
+            return false;
+          }
+          return true;
+        }
+      }
     },
-        surname: {
-            type: String,
-            required: true
-        }
-        age: {
-            type: Number,
-            required, true
-        }
-    */
     template: `
-    <div>
+      <div>
         <h2>Props con Vuejs 2</h2>
         <p>{{ name }} {{ surname }}, Edad: {{ age }}</p>
-    </div>
-`
+      </div>
+    `
 });
